4005b965b1bcf3aeb217d3bf2491868df07b7780
iadr <israel.diaz@alumnos.uach.cl>
2017-06-01 21:06:21 -0400

    arreglo javascripts

--- PROYECTOS ECLIPSE/Proto1/WebContent/js/leyenda.js
+++ PROYECTOS ECLIPSE/Proto1/WebContent/js/leyenda.js
@@ -1,11 +1,1 @@
 
-$('.box').each(function(){
-	$(this).after('<span class="checkbox">');
-});
-$('.box').on('click',function(){
-	$(this).toggleClass('icon-checkbox-unchecked icon-checkbox-checked');
-})
-$('input').click(function(){
-  $('input:not(:checked)').children().toggleClass('icon-radio-unchecked icon-radio-checked2');
-  $('input:checked').children().toggleClass('icon-radio-unchecked icon-radio-checked2');
-});
